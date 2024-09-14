ce5a06ba89093c6fa029e6a6b81749a64c826f5d
rehman-batt <i210322@nu.edu.pk>
2024-09-14 17:31:04 +0500

    test case update

--- test_cases.py
+++ test_cases.py
@@ -1,24 +1,31 @@
 import numpy as np
 import pytest
-import joblib
+import pickle
  
 
 def test_prediction_output_type():
+
     sample_input = np.array([[1500, 3, 2, 2, 1]]) 
-    prediction = joblib.load('model.pkl').predict(sample_input)
-    
-    assert isinstance(prediction, np.ndarray)
-    assert isinstance(prediction[0], (int, float))
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        prediction = model.predict(sample_input)
+        
+        assert isinstance(prediction, np.ndarray)
+        assert isinstance(prediction[0], (int, float))
 
 def test_zero_input():
     sample_input = np.array([[0, 0, 0, 0, 0]])  
-    prediction = joblib.load('model.pkl').predict(sample_input)
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        prediction = model.predict(sample_input)
     
-    assert np.isfinite(prediction).all()
+        assert np.isfinite(prediction).all()
 
 def test_invalid_input_shape():
     sample_input = np.array([[1500, 3, 2]])  
     with pytest.raises(ValueError):
-        joblib.load('model.pkl').predict(sample_input)
+        with open('model.pkl', 'rb') as f:
+            model = pickle.load(f)
+            prediction = model.predict(sample_input)
 
 
